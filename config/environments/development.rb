befc5dca454dec3edc14491cba0350b51b7c2e35
Iryna Volyk <ira.volyk13@gmail.com>
2020-04-13 18:34:57 +0300

    config fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
   :port           => 587,
   :address        => 'smtp.mailgun.org',
-  :domain         => '',
+  :domain         => 'localhost:3000',
   :authentication => :plain,
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
